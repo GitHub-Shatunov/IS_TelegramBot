d11ce6095a3a10e5c49d9758c41dd1631606e691
Ivan Shatunov <3614121@mail.ru>
2023-11-17 12:17:18 +0300

    17.11.2023 12.17

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Ivan Vladimirovich Shatunov 11')
+    print_hi('Ivan Vladimirovich Shatunov 2233')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
